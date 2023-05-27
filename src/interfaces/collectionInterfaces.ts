04117e7f9356965279dab955147c2eee7fdeec67
AwuAwuu <nicholas_s20@mhs.istts.ac.id>
2023-05-27 23:45:41 +0700

    COL,LECTION DONE

---
+++ src/interfaces/collectionInterfaces.ts
@@ -0,0 +1,20 @@
+export interface IBodyCollection {
+	name: string;
+	description: string;
+}
+
+export interface IDataCollection {
+    name: string;
+    description: string;
+    userId: number;
+}
+
+export interface IQueryGetCollection {
+    id: number;
+}
+
+export interface IUpdateCollection {
+    id: number;
+    name: string;
+    description: string;
+}
