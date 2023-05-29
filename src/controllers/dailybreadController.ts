e76d772237612fb0126c6a110e48493b369fb39b
jje <jasonjonathan132@gmail.com>
2023-05-29 16:08:24 +0700

    alter user table

--- src/controllers/dailybreadController.ts
+++ src/controllers/dailybreadController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express"
 
+interface IDailyBread {
+  bibleId: string;
+  verseId: string;
+}
+
 export const getDailyBread = async (req: Request, res: Response): Promise<void> => {
   return;
+}
+
+async function getDetails(): Promise<IDailyBread> {
+  const randomId = Math.floor(Math.random() * 6) + 1;
+
+  const result = await prisma.dailyBread.findUnique({
+    where: {
+      id: randomId,
+    },
+  });
 }
