917b58897367a420849d885d0b54553b9e5a0894
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-05-30 12:17:17 +0700

    add upgrade account endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
 const router: Router = Router();
-import { login, logout, refreshToken, register } from '../controllers/authController';
+import { login, logout, refreshToken, register, upgradeAccount } from '../controllers/authController';
 
 router.post('/login', (req: Request, res: Response, next: NextFunction): void => {
     void login(req, res).catch(next);
@@ -13,6 +13,9 @@ router.get('/refresh-token', (req: Request, res: Response, next: NextFunction): 
 });
 router.get('/logout', (req: Request, res: Response, next: NextFunction): void => {
     void logout(req, res).catch(next);
+});
+router.put('/upgrade', (req: Request, res: Response, next: NextFunction): void => {
+    void upgradeAccount(req, res).catch(next);
 });
 
 export default router;  
